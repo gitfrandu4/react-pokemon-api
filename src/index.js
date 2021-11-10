8d7088bba389a650ab526925bd3fc4d25a08be06
Francisco J. López <frandufour@protonmail.com>
2021-11-10 00:33:58 +0000

    pokemon api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const axios = require('axios');
+
+class Pokemon extends Component {
+  constructor(props){
+    
+    super(props)
+
+    this.state = 
+      axios.get('https://pokeapi.co/api/v2/pokemon/1')
+        .then(function (response) {
+          // handle success
+          console.log(response);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+  }
+
+  componentDidMount(){
+    console.log("=>>>>" + this.state)
+  }
+
+  render() {
+    return (
+      <h2>¡Hazte con todos!</h2>
+      
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Pokemon />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
