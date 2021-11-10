4e863c59b7f13928002da0da70a071364b3c7471
Francisco J. López <frandufour@protonmail.com>
2021-11-10 16:48:38 +0000

    add correcciones javi

--- src/index.js
+++ src/index.js
@@ -1,44 +1,57 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-const axios = require("axios");
+// const axios = require("axios");
 
 const endpoint = "https://pokeapi.co/api/v2/pokemon/1";
 
-function getPokemon(endpoint){
-  return axios
-    .get(endpoint)
-    .then(function (response) {
-      console.log(response)
-      return response;
-    })
-    .catch(function (error) {
-    });
-}
+// function getPokemon(endpoint){
+//   return axios
+//     .get(endpoint)
+//     .then(function (response) {
+//       // console.log(response)
+//       return response.data;
+//     })
+//     .catch(function (error) {
+//     });
+// }
 
 class Pokemon extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-      isLoading: true,
-      name: null,
-    }
+			isLoading: true,
+			pokemon: null,
+		};
 	}
 
 	async componentDidMount() {
+		// let pokemonData = await getPokemon(endpoint);
+		const response = await fetch(endpoint);
+		const data = await response.json();
 
-    let pokemonData = await getPokemon(endpoint);
-
-    this.state = pokemonData.data;
-
-		console.log("======> " + pokemonData);
-		console.log("======> " + JSON.stringify(this.state.species));
-		console.log("======> " + this.state.species.name);
+		console.log(data);
+		this.setState({
+			isLoading: false,
+			pokemon: {
+				name: data.name,
+				avatar: data.sprites.front_shiny,
+			},
+		});
 	}
 
 	render() {
-		return <h2>¡Hazte con todos!</h2>;
+		if (this.state.isLoading === true) {
+			return <h2>Loading...</h2>;
+		}
+
+		return (
+			<div>
+				<h2>{this.state.pokemon.name}</h2>
+				<img src={this.state.pokemon.avatar} alt="foto pokemon" />
+			</div>
+		);
 	}
 }
 
